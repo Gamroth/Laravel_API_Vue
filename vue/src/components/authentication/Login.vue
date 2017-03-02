1435cbb03f550f8874783034791bcb869a91fe8f
Rafał Rogulski <rrogulski@animativ.pl>
2017-03-02 21:33:40 +0100

    create auth package

--- vue/src/components/authentication/Login.vue
+++ vue/src/components/authentication/Login.vue
@@ -47,7 +47,10 @@
 
           this.$http.post("http://localhost:8000/oauth/token", data)
             .then(response => {
-              console.log(response)
+              this.$auth.setToken(
+                response.body.access_token, 
+                response.body.expires_in + Date.now()
+              );
             })
 
             // stary sposob
--- vue/src/main.js
+++ vue/src/main.js
@@ -4,8 +4,10 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 import Router from './routes.js'
+import Auth from './packages/auth/Auth.js'
 
 Vue.use(VueResource)
+Vue.use(Auth)
 
 new Vue({
   el: '#app',
