45f8eaeef0a3cdaa87081baa9725fc0187907f91
Rafał Rogulski <rrogulski@animativ.pl>
2017-03-04 18:17:47 +0100

    addinf deleting product also from the dom

--- vue/src/components/product/Product.vue
+++ vue/src/components/product/Product.vue
@@ -3,6 +3,7 @@
     <div class="card">
       <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/a64fa624552369.5633627b8e849.jpg" alt="Thumbnail" class="card-img-top">
       <div class="card-block">
+        {{ productdata.user_id }} | {{ authenticatedUser.id }}
         <h6 class="card-title">
           {{ productdata.name }} 
         </h6>
@@ -14,7 +15,7 @@
           <a href="#" class="btn btn-success">Buy</a>
         </p>
         <p class="card-text d-flex flex-row justify-content-end" v-if="productdata.user_id == authenticatedUser.id">
-          <a href="#" class="btn btn-danger" role="button" @click="deleteProduct">Delete</a>
+          <a href="#" class="btn btn-danger" role="button" @click="$emit('delete-product')">Delete</a>
         </p>
       </div>
     </div>
@@ -33,34 +34,9 @@
 </style>
 
 <script>
-  import swal from 'sweetalert'
-
+  
   export default {
-    props: ['productdata', 'authenticatedUser'],
-
-    methods: {
-      deleteProduct () {
-        swal({
-            title: "Are you sure?",
-            text: "You will not be able to recover this product!",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            closeOnConfirm: false
-          },
-          function () {
-            this.$http.delete('api/products/' + this.productdata.id)
-              .then(response => {
-                console.log(response)
-
-                 swal("Deleted!", "Your product has been deleted.", "success");
-              })
-          }.bind(this)
-        );
-      }
-    }
-
+    props: ['productdata', 'authenticatedUser']
   }
   
 </script>
--- vue/src/components/product/Products.vue
+++ vue/src/components/product/Products.vue
@@ -4,6 +4,7 @@
     <div class="row">
       <my-product 
         v-for="product in products"
+        @delete-product="deleteProduct(product)"
         :authenticatedUser="authenticatedUser"
         :productdata="product">
       
@@ -14,6 +15,8 @@
 
 
 <script>
+  import swal from 'sweetalert'
+
   import Product from './Product.vue'
 
   export default {
@@ -38,6 +41,31 @@
         .then(response => {
           this.products = response.body
         })
+    }, 
+
+     methods: {
+      deleteProduct (product) {
+        swal({
+            title: "Are you sure?",
+            text: "You will not be able to recover this product!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            closeOnConfirm: false
+          },
+          function () {
+            this.$http.delete('api/products/' + product.id)
+              .then(response => {
+                let index = this.products.indexOf(product)
+
+                this.products.splice(index, 1)
+
+                swal("Deleted!", "Your product has been deleted.", "success");
+              })
+          }.bind(this)
+        );
+      }
     }
   }
 </script>
